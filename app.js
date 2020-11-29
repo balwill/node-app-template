a68f93a3061afd3415859ea2f0ef753631cdcdd0
Brian Alwill <balwill@bu.edu>
2020-11-28 22:20:32 -0500

    added dynamic lists

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { reduce } = require('async');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -7,13 +8,24 @@ const port = process.env.PORT || 5000
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let tasks = [];
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        tasks: tasks
+    });
+    console.log(tasks)
 })
 
 app.post('/addtask', (req, res) => {
-    console.log(req.body);
-    res.render('index')
+    let newTask = req.body.taskName
+    tasks.push(newTask)
+    res.redirect('/');
+
+})
+
+app.delete('/deletepost', (req,res) => {
+console.log(res)
 })
 
 app.listen(port, () => {
