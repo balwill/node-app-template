7de6f820f554dc9b927ed91d6966e558450aa09f
Brian Alwill <balwill@bu.edu>
2020-12-16 21:44:29 -0500

    Created project template

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+// Web Service and Body-Parser
 const express = require('express');
 const bodyParser = require('body-parser');
 const { reduce } = require('async');
 const app = express()
 const port = process.env.PORT || 5000
 
-
+// uses ejs template
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,46 +14,11 @@ let completedTaskArray = [];
 
 app.get('/', (req, res) => {
     res.render('index', {
-        taskArray: taskArray
+
     });
-    console.log(`Incomplete Tasks: ${taskArray}`)
-    console.log(`Complete Tasks: ${completedTaskArray}`)
-})
-
-app.post('/addtask', (req, res) => {
-    let newTask = req.body.taskName
-    taskArray.push(newTask)
-    res.redirect('/');
 
 })
 
-app.post('/deletetask', (req,res) => {
-    
-    let selectedTasks = req.body.completedTask;
-
-    console.log(req.body)
-
-    if (typeof selectedTasks == 'object') {
-        selectedTasks.forEach(selectedTask => {
-            // let alreadyCompleted = completedTaskArray.includes(selectedTask)
-            // if (!alreadyCompleted) {
-                completedTaskArray.push(selectedTask)
-                let indexOfTaskToDelete = taskArray.indexOf(selectedTask)
-                taskArray.splice(indexOfTaskToDelete, 1)
-            // }
-        });
-    } else {
-        // let alreadyCompleted = completedTaskArray.includes(selectedTasks)
-        // if (!alreadyCompleted) {
-            completedTaskArray.push(selectedTasks)
-            let indexOfTaskToDelete = taskArray.indexOf(selectedTasks)
-            taskArray.splice(indexOfTaskToDelete, 1)
-        // }
-    }
-
-    res.redirect('/')
-    
-})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
